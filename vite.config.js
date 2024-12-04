23a2f99043cb57356cb742fa44f26153a7eed9c3
Neeleshxd <115217555+Neeleshxd@users.noreply.github.com>
2024-12-04 13:46:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Riot_Web/', // adjust this if you're deploying to a subdirectory
-})
+  base: process.env.GITHUB_PAGES ? '/Riot_Web/' : '/', // Use the base path only for GitHub Pages
+});
